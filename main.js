0494e18ec61862238604e01166db7f6a3c60768e
paul <545292428@qq.com>
2017-03-08 20:55:48 +0800

    Signed-off-by: paul <545292428@qq.com>

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ cc.game.onStart = function(){
     // Adjust viewport meta
     cc.view.adjustViewPort(true);
     // Setup the resolution policy and design resolution size
-    cc.view.setDesignResolutionSize(640, 960, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.setDesignResolutionSize(640, 960, cc.ResolutionPolicy.FIXED_HEIGHT);
     // The game will be resized when browser size change
     cc.view.resizeWithBrowserSize(true);
     //load resources
